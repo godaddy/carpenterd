ee09d3741af98dea1bb8d4df796e27fa48efc99e
Jarrett Cruger <jcrugzz@gmail.com>
2017-10-04 14:19:38 -0400

    [wip] testing

---
+++ test/fixtures/heads.js
@@ -0,0 +1,24 @@
+exports.missing = {
+  'my-client-side-package': [{
+    name: 'my-client-side-package',
+    version: '6.6.8',
+    env: 'test',
+    locale: 'it'
+  }, {
+    name: 'my-client-side-package',
+    version: '6.7.0',
+    env: 'test',
+    locale: 'en-US'
+  }],
+  'my-other-client-side-package': [{
+    name: 'my-other-client-side-package',
+    version: '3.4.4',
+    env: 'test',
+    locale: 'it'
+  }, {
+    name: 'my-client-side-package',
+    version: '3.4.5',
+    env: 'test',
+    locale: 'en-US'
+  }]
+}
