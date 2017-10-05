7203b55bf049a231e339ffa003f0c3926993e0ee
Jarrett Cruger <jcrugzz@gmail.com>
2017-10-05 12:27:08 -0400

    [test] write tests

--- test/lib/preboots/scheduler.test.js
+++ test/lib/preboots/scheduler.test.js
@@ -10,9 +10,10 @@ const heads = require('../../fixtures/heads');
 const path = require('path');
 
 describe('Scheduler', function () {
-
+  this.timeout(5E4);
   let app = require('../../../lib');
   let sandbox;
+  let scheduler;
 
   before(function (done) {
     app.start({
@@ -23,16 +24,19 @@ describe('Scheduler', function () {
       config: {
         file: path.join(__dirname, '..', '..', 'config.json'),
         overrides: {
-          http: 0
+          http: 0,
+          scheduler: {
+            topic: 'testing'
+          }
         }
       }
     }, function (err, application) {
+      if (err) return done(err);
       app = application;
       app.nsq = app.nsq || {};
       app.nsq.writer = app.nsq.writer || new Writer();
-      console.log(app.scheduler);
       app.scheduler.nsq = app.nsq;
-      scheduler = app.scheduler
+      scheduler = app.scheduler;
       done();
     });
   });
@@ -48,16 +52,65 @@ describe('Scheduler', function () {
 
   afterEach(function () {
     sandbox.restore();
-  })
+  });
+
+  it('setInterval should schedule a job that completes', function (done) {
+    sandbox.stub(scheduler.models.PackageCache, 'findAll').yieldsAsync(null, clone(packages));
+    const headStub = sandbox.stub(scheduler.models.BuildHead, 'findAll');
+    headStub.onCall(0).yieldsAsync(null, clone(heads.same['my-client-side-package']));
+    headStub.onCall(1).yieldsAsync(null, clone(heads.same['my-other-client-side-package']));
+
+    scheduler.once('scheduled', (err, counts) => {
+      const keys = Object.keys(counts);
+      assume(keys.length).equals(2);
+      keys.forEach(k => {
+        assume(counts[k]).equals(0);
+      });
+      scheduler.clear('test');
+      done();
+    });
+    scheduler.setInterval('test', 1000);
+  });
+
+  it('clear should clear any setIntervals that have been called', function () {
+    scheduler.setInterval('test');
+    scheduler.setInterval('prod');
+    scheduler.setInterval('dev');
+    assume(Array.from(scheduler.intervals.keys()).length).equals(3);
+    scheduler.clear();
+    assume(Array.from(scheduler.intervals.keys()).length).equals(0);
+  });
 
   it('should trigger a single build for each package when running schedule', function (done) {
-    sandbox.stub(scheduler, 'packages').yieldsAsync(null, clone(packages));
-    const headStub = sandbox.stub(scheduler.bffs.heads);
-    headStub.onCall(0).yieldsAsync(null, heads.missing['my-client-side-package']);
-    headStub.onCall(1).yieldsAsync(null, heads.missing['my-other-client-side-package']);
+    sandbox.stub(scheduler.models.PackageCache, 'findAll').yieldsAsync(null, clone(packages));
+    const headStub = sandbox.stub(scheduler.models.BuildHead, 'findAll');
+    headStub.onCall(0).yieldsAsync(null, clone(heads.missing['my-client-side-package']));
+    headStub.onCall(1).yieldsAsync(null, clone(heads.missing['my-other-client-side-package']));
 
     scheduler.schedule('test', function (err, counts) {
-      console.dir(arguments);
+      assume(err).is.falsey();
+      const keys = Object.keys(counts);
+      assume(keys.length).equals(2);
+      keys.forEach(k => {
+        assume(counts[k]).equals(1);
+      });
+      done();
+    });
+  });
+
+  it('should trigger zero builds when there are no behind packages', function (done) {
+    sandbox.stub(scheduler.models.PackageCache, 'findAll').yieldsAsync(null, clone(packages));
+    const headStub = sandbox.stub(scheduler.models.BuildHead, 'findAll');
+    headStub.onCall(0).yieldsAsync(null, clone(heads.same['my-client-side-package']));
+    headStub.onCall(1).yieldsAsync(null, clone(heads.same['my-other-client-side-package']));
+
+    scheduler.schedule('test', function (err, counts) {
+      assume(err).is.falsey();
+      const keys = Object.keys(counts);
+      assume(keys.length).equals(2);
+      keys.forEach(k => {
+        assume(counts[k]).equals(0);
+      });
       done();
     });
   });
